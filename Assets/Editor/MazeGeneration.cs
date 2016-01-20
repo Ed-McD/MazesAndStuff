2d56fb93230cd0b3d8a268679f6ae0fa9374e5b3
Ed-McD <edard_666@hotmail.co.uk>
2016-01-20 01:09:30 +0000

    The Basics

---
+++ Assets/Editor/MazeGeneration.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class MazeGeneration{
+
+
+    stats _stats;
+
+
+    public void Generate(stats _params)
+    {
+        Debug.Log("Generating...");
+        _stats = _params;
+    }
+    
+}
---
+++ Assets/Editor/MazeMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+
+public struct stats
+{
+    
+    public int mazeWidth, mazeDepth;
+    public float wallThickness, wallHeight;
+    bool Prim;
+}
+
+public class MazeMenu : EditorWindow
+{
+    public stats param = new stats();
+    public MazeGeneration generator = new MazeGeneration();
+    private Vector2 scrollPos;
+
+    [MenuItem("Custom Tools/Maze Generator")]
+    private static void OpenWindow()
+    {
+        EditorWindow.GetWindow<MazeMenu>(typeof(MazeMenu));
+    }
+
+    [MenuItem("Tools/Maze Generator", true)]
+    private static bool showEditorValidator()
+    {
+        return true;
+    }
+
+   
+    void OnGUI()
+    {
+        
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        {
+            
+            param.mazeWidth= EditorGUILayout.IntSlider("Maze Width", param.mazeWidth, 0, 50);
+
+            param.mazeDepth= EditorGUILayout.IntSlider("Maze Depth", param.mazeDepth, 0, 50);
+            if ( GUILayout.Button("Generate"))
+            {
+                generator.Generate(param);
+            }
+        }
+        EditorGUILayout.EndScrollView();        
+    }
+    
+        
+}
